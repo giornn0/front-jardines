442eda3994e4e65c4fe2bb9da8c35facbedfd84d
giornn0 <shaaamsuu@gmail.com>
2022-01-22 17:48:12 -0300

    feature projects routes

---
+++ pages/colegios/editar.js
@@ -0,0 +1,10 @@
+import {ColegioCard} from '../../components/ColegioCard'
+
+export default function FormColegio(){
+  return (
+    <>
+    <h1>CREANDO COLEGIOS PEPITO SE FUE A LA GUERRA</h1>
+    <ColegioCard></ColegioCard>
+    </>
+  )
+}
---
+++ pages/usuario/configuracion.js
@@ -0,0 +1,5 @@
+export default function ConfiguracionUsuario(){
+  return (
+    <h1>Configurando usuario!</h1>
+  )
+}
