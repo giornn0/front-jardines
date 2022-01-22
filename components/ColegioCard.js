4684716a38d764f55a3da3e3d3b646cda4f09ae6
giornn0 <shaaamsuu@gmail.com>
2022-01-22 20:25:56 -0300

    background gradient defined, testing login disposition

--- components/ColegioCard.js
+++ components/ColegioCard.js
@@ -17,7 +17,7 @@ const MaterialUISwitch = styled(Switch)(({ theme }) => ({
       color: '#fff',
       transform: 'translateX(22px)',
       '& .MuiSwitch-thumb:before': {
-        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="../public/logo2.png" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
           '#fff',
         )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
       },
@@ -179,6 +179,8 @@ const AntSwitch = styled(Switch)(({ theme }) => ({
 }));
 
 export const ColegioCard= ()=> {
+  console.log(encodeURIComponent(
+    '#fff'))
   return (
     <FormGroup>
       <FormControlLabel
@@ -193,11 +195,6 @@ export const ColegioCard= ()=> {
         control={<IOSSwitch sx={{ m: 1 }} defaultChecked />}
         label="iOS style"
       />
-      <Stack direction="row" spacing={1} alignItems="center">
-        <Typography>Off</Typography>
-        <AntSwitch defaultChecked inputProps={{ 'aria-label': 'ant design' }} />
-        <Typography>On</Typography>
-      </Stack>
     </FormGroup>
   );
 }
--- pages/login.js
+++ pages/login.js
@@ -6,19 +6,20 @@ const image1 = "https://static.toiimg.com/thumb/msid-80300740,width-400,resizemo
 
 const Login = () => {
   return (
-    <div className='flex w-full justify-center items-center'>
-      <div className='flex flex-col md:flex-row w-full'>
-        <div className='flex flex-1 justify-center'>
-          <ColegioCard></ColegioCard>
+    <div className='flex w-full justify-center items-center bg-color h-screen'>
+      <div className='flex basis-1/2 '>
+        <div className="flex flex-col border-rose-800">
+        <input class="m-50 border-2 border-rose-600 ..."></input>
+        <input class="m-50 border-2 border-green-300 ..."></input>
+        <input class="m-50 border-2 border-rose-600 ..."></input>
         </div>
-        <div className='flex flex-1 justify-center'>
-          <Image 
-            src={loginImage}
-            alt="portrait"
-            width="200"
-            height="200"
-          />
-        </div>
+      </div>
+      <div className='flex-1/2'>
+        <Image
+        className="opacity-40 w-full h-full"
+        src={loginImage}
+        alt="portrait"
+        ></Image>
       </div>
     </div>
   );
